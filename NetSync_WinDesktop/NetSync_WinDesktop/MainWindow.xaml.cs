f29d0cc3ea69fab69010669d02cbd682cc9bc3b9
Kadyrov96 <kadyrov96@mail.ru>
2017-03-12 22:13:20 +0300

    Added new whole desktop app for sync. Created just sync profiles menu and added design samples. No implementation of logic inside.

---
+++ NetSync_WinDesktop/NetSync_WinDesktop/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetSync_WinDesktop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dirSelect_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void settings_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void startSync_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void localScan_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void import_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void export_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
