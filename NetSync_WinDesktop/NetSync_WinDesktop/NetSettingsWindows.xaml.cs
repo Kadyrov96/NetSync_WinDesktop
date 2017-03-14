681b420720f97ae0536792a18a813fad2e489148
Kadyrov96 <kadyrov96@mail.ru>
2017-03-14 18:15:29 +0300

    Class of added NetSettingsWindow

---
+++ NetSync_WinDesktop/NetSync_WinDesktop/NetSettingsWindows.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace NetSync_WinDesktop
+{
+    /// <summary>
+    /// Логика взаимодействия для NetSettingsWindows.xaml
+    /// </summary>
+    public partial class NetSettingsWindows
+    {
+        public NetSettingsWindows()
+        {
+            InitializeComponent();
+
+            double screenHeight = SystemParameters.FullPrimaryScreenHeight;
+            double screenWidth = SystemParameters.FullPrimaryScreenWidth;
+            Top = (screenHeight - Height) / 2 + 150;
+            Left = (screenWidth - Width) / 2 + 150;
+            ip.IsReadOnly = true;
+            hostname.IsReadOnly = true;
+        }
+    }
+}
