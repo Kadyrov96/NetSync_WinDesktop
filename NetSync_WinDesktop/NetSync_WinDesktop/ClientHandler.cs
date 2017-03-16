026bda428b74dcc76d8450e322206509a9ab8348
Kadyrov96 <kadyrov96@mail.ru>
2017-03-16 13:42:51 +0300

    New class that implements all logic of servicing mobile device, including connection and synchronisation.

---
+++ NetSync_WinDesktop/NetSync_WinDesktop/ClientHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NetSync_WinDesktop
+{
+    class ClientHandler
+    {
+        private TcpClient client;
+        private IStreamHandler streamHandler;
+        private string clientProfile;
+        private Synchroniser syncService;
+
+        ClientHandler(TcpClient _tcpClient)
+        {
+            client = _tcpClient;
+            streamHandler = new IOStreamHandler(new SslStream(client.GetStream(), false));
+        }
+
+        void ServeClient()
+        {
+            clientProfile = streamHandler.ReceiveString();
+
+            syncService = new Synchroniser(new FolderHandler(clientProfile));
+            syncService.CheckLocalChanges();
+
+            streamHandler.ReceiveData(Directory.GetCurrentDirectory());
+            streamHandler.SendData(syncService.CompareDevicesSyncData());
+
+            //TO DO - отправка и прием файлов // 
+        }
+
+    }
+}
