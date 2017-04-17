7caebf3c94255dd6721c55a53b5c2fdd0db2816a
Kadyrov96 <kadyrov96@mail.ru>
2017-04-17 10:32:53 +0300

    Added class for interaction with profiles: addind, deleting, loading and saving to data file.

---
+++ NetSync_WinDesktop/NetSync_WinDesktop/SyncProfilesHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NetSync_WinDesktop
+{
+    class SyncProfilesHandler
+    {
+        static string syncProfilesStore = Directory.GetCurrentDirectory() + "profiles.dat";
+        static List<SyncProfile> availableProfilesList;
+        static List<SyncProfile> selectedProfilesList;
+
+        static public void AddNewProfile(string name, string path)
+        {
+            availableProfilesList = new List<SyncProfile>();
+            availableProfilesList.Add(new SyncProfile(name, path, DateTime.Now.ToString()));
+            SaveProfiles();
+            LoadProfiles();
+        }
+
+        static public void DeleteProfile()
+        {
+
+        }
+
+        static void LoadProfiles()
+        {
+            string[] syncProfilesArray = File.ReadAllLines(syncProfilesStore);
+            availableProfilesList = new List<SyncProfile>();
+            foreach (var profile in syncProfilesArray)
+            {
+                char delimiter = '_';
+                string[] substrings = profile.Split(delimiter);
+                availableProfilesList.Add(new SyncProfile(substrings[0], substrings[1], substrings[2]));
+            }
+        }
+
+        static void SaveProfiles()
+        {
+            StreamWriter profileWriter;
+            if (File.Exists(syncProfilesStore))
+            {
+                profileWriter = new StreamWriter(syncProfilesStore);
+
+                foreach(var profile in availableProfilesList)
+                    profileWriter.WriteLine(profile.ProfileName + "_" +
+                        profile.ProfileSyncFolderPath + "_" + profile.SyncDateTime);
+            }
+            else
+            {
+                FileStream synProfilesStoreCreator = File.Create(syncProfilesStore);
+                profileWriter = new StreamWriter(syncProfilesStore);
+
+                foreach (var profile in availableProfilesList)
+                    profileWriter.WriteLine(profile.ProfileName + "_" +
+                        profile.ProfileSyncFolderPath + "_" + profile.SyncDateTime);
+            }
+            profileWriter.Close();
+            profileWriter.Dispose();
+        }
+    }
+}
