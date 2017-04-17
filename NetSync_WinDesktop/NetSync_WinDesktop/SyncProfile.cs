2c2c83d9cbeb76c81af95293afa3a3606992f840
Kadyrov96 <kadyrov96@mail.ru>
2017-04-17 10:28:03 +0300

    Added new class that describes synchronization profile: name, folderPath and date of sync.

---
+++ NetSync_WinDesktop/NetSync_WinDesktop/SyncProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NetSync_WinDesktop
+{
+    class SyncProfile
+    {
+        public SyncProfile(string name, string path, string dateTime)
+        {
+            ProfileName = name;
+            ProfileSyncFolderPath = path;
+            SyncDateTime = dateTime;
+        }
+
+        public string ProfileName { get; set; }
+        public string ProfileSyncFolderPath { get; set; }
+        public string SyncDateTime { get; set; }
+    }
+}
