c8e3f1028648766e75fe6217c33cf8e1d452aa78
Kadyrov96 <kadyrov96@mail.ru>
2017-04-17 10:34:30 +0300

    Class for ProfileAddingWindow.

---
+++ NetSync_WinDesktop/NetSync_WinDesktop/ProfileAddingWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NetSync_WinDesktop
+{
+    /// <summary>
+    /// Логика взаимодействия для ProfileAddingWindow.xaml
+    /// </summary>
+    public partial class ProfileAddingWindow
+    {
+        public ProfileAddingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
