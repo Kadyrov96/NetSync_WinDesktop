4ac07ae0e2b4e52d9b83096b36b63f8ed601f4de
Руслан Кадыров < >
2017-04-30 17:52:11 +0300

    Added functionality of adding new profile, deleting profile, validation of entering the same name or folderpath that is already existed. Also refactored whole class.

--- NetSync_WinDesktop/NetSync_WinDesktop/SyncProfilesWindow.xaml.cs
+++ NetSync_WinDesktop/NetSync_WinDesktop/SyncProfilesWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using System.Windows;
-using System.ComponentModel;
-using System.Windows.Data;
+using System;
+using System.Collections.Generic;
 
 namespace NetSync_WinDesktop
 {
@@ -10,18 +10,17 @@ namespace NetSync_WinDesktop
     /// </summary>
     public partial class SyncProfilesWindow
     {
+        private List<SyncProfile> profileList;
         public SyncProfilesWindow()
         {
             InitializeComponent();
-            if(File.Exists(Directory.GetCurrentDirectory() + @"\profiles.dat"))
+            if (File.Exists(SyncProfilesHandler.syncProfilesStore))
             {
                 SyncProfilesHandler.LoadProfiles();
-                foreach (var item in SyncProfilesHandler.AvailableProfilesList)
-                {
-                    profilesListView.Items.Add(item);
-                }
+                RefreshListView();
             }
-
+            else
+                SyncProfilesHandler.LoadEmptyProfiles();
 
             double screenHeight = SystemParameters.FullPrimaryScreenHeight;
             double screenWidth = SystemParameters.FullPrimaryScreenWidth;
@@ -33,15 +32,51 @@ namespace NetSync_WinDesktop
         {
 
         }
-
         private void AddNewProfile_Btn_Click(object sender, RoutedEventArgs e)
         {
-            var addNewProfileWindow = new ProfileAddingWindow();
-            addNewProfileWindow.ShowDialog();
-            //Application.Current.Windows.Contain(ProfileAddingWindow);
-            //this.profilesListView
+            ProfileAddingWindow addingWindow = new ProfileAddingWindow();
+            addingWindow.DataChanged += addingWindow_DataChanged;
+            addingWindow.Show();
+        }
 
-            //listView1.Items.Add(new FileItem(syncDirPath + localNamesList[i], imageDel.Source, imageWait.Source, 140));
+        private void DeleteProfile_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in profilesListView.SelectedItems)
+            {
+                SyncProfile selectedProfile = (SyncProfile)item;
+                SyncProfilesHandler.DeleteProfile(selectedProfile.ProfileName);
+            }
+            RefreshListView();
+        }
+
+        private void RefreshListView()
+        {
+            profileList = SyncProfilesHandler.AvailableProfilesList;
+            profilesListView.Items.Clear();
+            foreach (var item in profileList)
+                profilesListView.Items.Add(item);
+        }
+        void addingWindow_DataChanged(object sender, EventArgs e)
+        {
+            RefreshListView();
+        }
+
+        private void SyncSelectedProfiles_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in profilesListView.SelectedItems)
+                SyncProfilesHandler.SelectedProfilesList.Add((SyncProfile)item);
+
+            if (SyncProfilesHandler.SelectedProfilesList.Count != 0)
+            {
+                MessageBox.Show("Selected profiles are in queue now. Please, start sync service in main window.",
+                    "Succesful choice of profiles", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("No profiles were selected. Please, select profile or create a new one.",
+                    "Unsuccesful choice of profiles", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
