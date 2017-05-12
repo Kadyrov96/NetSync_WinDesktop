85491c6718660718c2cc0de330b472722653f728
Kadyrov96 <kadyrov96@mail.ru>
2017-05-12 15:28:28 +0300

    Added TLS authentication.

--- NetSync_WinDesktop/NetSync_WinDesktop/ClientHandler.cs
+++ NetSync_WinDesktop/NetSync_WinDesktop/ClientHandler.cs
@@ -1,21 +1,40 @@
-﻿using System.Net.Sockets;
-using System.Net.Security;
+﻿using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 namespace NetSync_WinDesktop
 {
-    class ClientHandler
+    public class ClientHandler
     {
         TcpClient client;
         IStreamHandler streamHandler;
         Synchroniser syncService;
+        X509Certificate2 serverCertificate;
 
-        ClientHandler(TcpClient _tcpClient)
+        public ClientHandler(TcpClient _tcpClient)
         {
             client = _tcpClient;
-            streamHandler = new IOStreamHandler(new SslStream(client.GetStream(), false));
+            SslStream sslStream = new SslStream(client.GetStream(), false);
+            //serverCertificate = new X509Certificate2(@"C:\Users\Admin\Desktop\123.pfx", "vbhs456");
+            try
+            {
+                //sslStream.AuthenticateAsServer(serverCertificate, false, SslProtocols.Tls12, true);
+                streamHandler = new IOStreamHandler(sslStream);
+            }
+            catch (AuthenticationException e)
+            {
+                System.Windows.MessageBox.Show("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    System.Windows.MessageBox.Show("Inner exception: {0}", e.InnerException.Message);
+                }
+                System.Windows.MessageBox.Show("Authentication failed - closing the connection.");
+                sslStream.Close();
+            }
         }
 
-        void ServeClient()
+        public void ServeClient()
         {
             string profileName = streamHandler.ReceiveString();
             SyncProfile clientProfile = SyncProfilesHandler.SelectedProfilesList.Find(
