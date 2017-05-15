ebc511b8a0c1dbf8c45d5dc150b61f2ed8a887bd
Kadyrov96 <kadyrov96@mail.ru>
2017-05-15 22:57:58 +0300

    Removed part of the class that was responible for connection to the TCP_Server.cs.

--- NetSync_WinDesktop/NetSync_WinDesktop/ClientHandler.cs
+++ NetSync_WinDesktop/NetSync_WinDesktop/ClientHandler.cs
@@ -1,37 +1,15 @@
 ﻿using System.Net.Security;
-using System.Net.Sockets;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
 
 namespace NetSync_WinDesktop
 {
     public class ClientHandler
     {
-        TcpClient client;
         IStreamHandler streamHandler;
         Synchroniser syncService;
-        X509Certificate2 serverCertificate;
 
-        public ClientHandler(TcpClient _tcpClient)
+        public ClientHandler(SslStream sslStream)
         {
-            client = _tcpClient;
-            SslStream sslStream = new SslStream(client.GetStream(), false);
-            //serverCertificate = new X509Certificate2(@"C:\Users\Admin\Desktop\123.pfx", "vbhs456");
-            try
-            {
-                //sslStream.AuthenticateAsServer(serverCertificate, false, SslProtocols.Tls12, true);
-                streamHandler = new IOStreamHandler(sslStream);
-            }
-            catch (AuthenticationException e)
-            {
-                System.Windows.MessageBox.Show("Exception: {0}", e.Message);
-                if (e.InnerException != null)
-                {
-                    System.Windows.MessageBox.Show("Inner exception: {0}", e.InnerException.Message);
-                }
-                System.Windows.MessageBox.Show("Authentication failed - closing the connection.");
-                sslStream.Close();
-            }
+            streamHandler = new IOStreamHandler(sslStream);
         }
 
         public void ServeClient()
